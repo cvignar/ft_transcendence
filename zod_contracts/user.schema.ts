b4f67fca3f0757102ba65e9192f830e6d7d8928e
ebeiline <ebeiline@students.42wolfsburg.de>
2023-09-07 14:49:32 +0200

    user_schema added

---
+++ zod_contracts/user.schema.ts
@@ -0,0 +1,31 @@
+import { z } from 'nestjs-zod/z';
+import { createZodDto } from 'nestjs-zod';
+
+const UserSchema = z.object({
+	id: z.number().int(),
+	username: z.string(),
+	email: z.string().email({message: 'invalid email'}),
+	avatar: z.string().max(65_000), //optional
+	gamesWon: z.number().int(),
+	gamesLost: z.number().int(),
+	gamesPlayed: z.number().int(),
+	rank: z.number().int(),
+	score: z.number().int(),
+	added: z.array(z.number().int()),
+	adding: z.array(z.number().int()),
+	friends: z.array(z.number().int()),
+	blocked: z.array(z.number().int()),
+	blocking: z.array(z.number().int()),
+	blocks: z.array(z.number().int()),
+	// @Exclude()
+	hash: z.string(),
+	// @Exclude()
+	hashedRtoken: z.string(),
+});
+
+
+
+export namespace CreateUser {
+	export class Request extends createZodDto(UserSchema) {}
+}
+
