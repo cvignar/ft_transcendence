e13f441839f5bc0df01d1bb028aec124bc31d897
ebeiline <ebeiline@students.42wolfsburg.de>
2023-09-11 14:56:53 +0200

    added update function to user

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -10,24 +10,47 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User as UserModel } from '@prisma/client';
-import { CreateUser } from '../../../zod_contracts/user.schema';
+import { CreateUser, UpdateAvatar, UpdateEmail, UpdateUsername } from '../../../zod_contracts/user.schema';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  
+  @Get('user')
+  async listAllUsers() {
+    return await this.userService.listAllUsers();
+  }
+
+
+  @Get('user/all')
+  async getAllUsers() {
+    return await this.userService.getAllUsers();
+  }
 
   @Get('user/:id')
   async getUserById(@Param('id', ParseIntPipe) id: number) {
     return await this.userService.getUserById(id);
   }
 
-  @Get('user')
-  async listAllUsers() {
-    return await this.userService.listAllUsers();
-  }
-
   @Post('user/create')
   async signupUser(@Body() userData: CreateUser.Request): Promise<UserModel> {
     return this.userService.createUser(userData);
   }
+
+  @Post('user/update/name')
+  async updateUsername(@Body() updateData: UpdateUsername.Request): Promise<UserModel> {
+    return this.userService.updateUsername(updateData);
+  }
+
+  @Post('user/update/email')
+  async updateEmail(@Body() updateData: UpdateEmail.Request): Promise<UserModel> {
+	return this.userService.updateEmail(updateData);
+  }
+
+  @Post('user/update/avatar')
+  async updateAvatar(@Body() updateData: UpdateAvatar.Request): Promise<UserModel> {
+	return this.userService.updateAvatar(updateData);
+  }
+  
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User, Prisma } from '@prisma/client';
 // import { ChannelService } from './channel.service';
-import { CreateUser } from '../../../zod_contracts/user.schema'
+import { CreateUser, UpdateAvatar, UpdateEmail, UpdateUsername } from '../../../zod_contracts/user.schema'
 
 @Injectable()
 export class UserService {
@@ -26,32 +26,9 @@ export class UserService {
 	async listAllUsers() {
 		return await this.prisma.user.findMany();
 	}
-//   async user(
-//     userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-//   ): Promise<User | null> {
-//     return this.prisma.user.findUnique({
-//       where: userWhereUniqueInput,
-//     });
-//   }
 
-//   async users(params: {
-//     skip?: number;
-//     take?: number;
-//     cursor?: Prisma.UserWhereUniqueInput;
-//     where?: Prisma.UserWhereInput;
-//     orderBy?: Prisma.UserOrderByWithRelationInput;
-//   }): Promise<User[]> {
-//     const { skip, take, cursor, where, orderBy } = params;
-//     return this.prisma.user.findMany({
-//       skip,
-//       take,
-//       cursor,
-//       where,
-//       orderBy,
-//     });
-//   }
 
-  async createUser(userData: CreateUser.Request): Promise<User> {
+  	async createUser(userData: CreateUser.Request): Promise<User> {
 	try{
 		const user = await this.prisma.user.create({
 			data: {
@@ -69,20 +46,85 @@ export class UserService {
 	}
   }
 
-  async updateUser(params: {
-    where: Prisma.UserWhereUniqueInput;
-    data: Prisma.UserUpdateInput;
-  }): Promise<User> {
-    const { where, data } = params;
-    return this.prisma.user.update({
-      data,
-      where,
-    });
-  }
+  	async getAllUsers() {
+	//returns a record of all the users, ordered by id in acending order
+	try {
+		const userList = await this.prisma.user.findMany({
+			orderBy: 
+			{
+				id: 'asc',
+			},
+			select: {
+				id: true,
+				username: true,
+				email: true,
+				avatar: true,
+			}
+		});
+		return userList;
+	}
+	catch (error) {
+		return `getAllUsers error`;
+	}
+}
 
-  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+//   async updateUser(params: {
+//     where: Prisma.UserWhereUniqueInput;
+//     data: Prisma.UserUpdateInput;
+//   }): Promise<User> {
+//     const { where, data } = params;
+//     return this.prisma.user.update({
+//       data,
+//       where,
+//     });
+//   }
+
+ 	async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
     return this.prisma.user.delete({
       where,
     });
   }
+
+/*	UPDATE	*/
+
+	async updateUsername(updateData: UpdateUsername.Request): Promise<User> {
+		try {
+			const updateUser = await this.prisma.user.update({
+				where: { id: updateData.id },
+				data: { username: updateData.username },
+			});
+			return updateUser;
+		} catch (error) {
+			console.log(`updateUsername error: ${error}`);
+		}
+	}
+
+	async updateEmail(updateData: UpdateEmail.Request): Promise<User> {
+		try {
+			const updateUser = await this.prisma.user.update({
+				where: { id: updateData.id },
+				data: { email: updateData.email },
+			});
+			return updateUser;
+		} catch (error) {
+			console.log(`updateEmail error: ${error}`);
+		}
+	}
+
+	async updateAvatar(updateData: UpdateAvatar.Request): Promise<User> {
+		try {
+			const updateUser = await this.prisma.user.update({
+				where: { id: updateData.id },
+				data: { avatar: updateData.avatar },
+			});
+			return updateUser;
+		}
+		catch (error) {
+			console.log(`updateAvatar error: ${error}`);
+		}
+	}
+
+
+	
 }
+
