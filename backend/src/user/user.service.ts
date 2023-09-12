45790c68a7529300a93536d9201bfb1e48cba5ea
cvignar <cvignar@c2s3d6.42wolfsburg.de>
2023-09-12 15:50:55 +0200

    old channel with contracts added

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateUser } from '../../../contracts/user.schema';
+import { CreateUser } from '../../../contracts/user.schema--';
 
 @Injectable()
 export class UserService {
