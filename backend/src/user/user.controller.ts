07134aa4c235062275dec9581f135591558aca3c
cvignar <cvignar@c2s3d6.42wolfsburg.de>
2023-09-12 15:14:22 +0200

    front fake form, backend user start

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,19 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post, UsePipes } from '@nestjs/common';
+import { ZodValidationPipe } from 'nestjs-zod';
 import { UserService } from './user.service';
-
+import { CreateUser } from '../../../contracts/user.schema';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from '@prisma/client';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @UsePipes(ZodValidationPipe)
+  //@ApiTags('pong72')
   @Post('create')
+  //@ApiOperation({ summary: 'Create user' })
   async createUser(
     @Body()
-    userData: {
-      username: string;
-      email: string;
-      hash: string;
-      id42: string;
-    },
-  ) {
+    userData: CreateUser.Request,
+  ): Promise<User> {
     return this.userService.createUser(userData);
   }
 }
