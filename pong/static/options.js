866de89755affca534ba0ff67c5cd9d0217338f0
mclam <mclam@student.42wolfsburg.de>
2023-09-13 12:16:37 +0200

    changed calculation_period

--- pong/static/options.js
+++ pong/static/options.js
@@ -38,7 +38,7 @@ PongOptions.ball_startSpeed = 100 * _b.line_width;
 PongOptions.ball_StartClicksForSpeedup = 12;
 PongOptions.ball_speedUp = 1.2;
 PongOptions.ball_speedUpMax = 6;
-PongOptions.calculation_period = 1000 / 30;
+PongOptions.calculation_period = 1000 / 60;
 export class ControlOptions extends Options {
 }
 _c = ControlOptions;
