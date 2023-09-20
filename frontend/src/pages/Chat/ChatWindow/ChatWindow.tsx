148e15ac501861a276f1aeaa48b9651efe01674e
cvignar <cvignar@c2s3d6.42wolfsburg.de>
2023-09-20 23:05:40 +0200

    modal for creating channel added

--- frontend/src/pages/Chat/ChatWindow/ChatWindow.tsx
+++ frontend/src/pages/Chat/ChatWindow/ChatWindow.tsx
@@ -3,7 +3,7 @@ import styles from './ChatWindow.module.css';
 import Headling from '../../../components/Headling/Headling';
 import { ChannelShortInfo } from '../../../components/ChannelShortInfo/ChannelShortInfo';
 import Input from '../../../components/Input/Input';
-import { useEffect, useRef, useState } from 'react';
+import { createRef, useEffect, useRef, useState } from 'react';
 import { CreateMessage } from '../../../interfaces/createMessage.interface';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../../store/store';
@@ -17,10 +17,8 @@ function ChatWindow({ data }: ChatWindowProps) {
 	const [value, setValue] = useState<{messageInput: string}>({messageInput: ''});
 	const messages: Message[] = useSelector((s: RootState) => s.channel.messages);
 
-	const div = useRef(null);
-
 	const onSendMessage = (event: React.KeyboardEvent) => {
-		if (event.key == 'Enter' && message != '') {
+		if (event.key == 'Enter' && /\S/.test(message)) {
 			const newMessage: CreateMessage = {
 				message: message,
 				email: email,
@@ -32,30 +30,23 @@ function ChatWindow({ data }: ChatWindowProps) {
 		}
 	};
 
-	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+	const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
 		setValue({messageInput: event.target.value});
 		setMessage(event.target.value);
 	};
 
 	useEffect(() => {
-		console.log(data.id);
 		dispatch(channelActions.getMessages(data.id));
 	}, [dispatch, data.id]);
 
-	useEffect(() => {
-		console.log(messages);
-	}, [messages]);
-
-	useEffect(() => {
-		div.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-	}, []);
 	return (
 		<div className={styles['window']}>
 			<div className={styles['head']}>
 				<ChannelShortInfo appearence='chat' props={data}/>
 			</div>
+			<hr/>
 			<div className={styles['chat-area']}>
-				<div className={styles['messages']} ref={div}>
+				<div className={styles['messages']}>
 					{messages?.map(message => (
 						<MessageHolder
 							key={message.id}
@@ -67,7 +58,14 @@ function ChatWindow({ data }: ChatWindowProps) {
 				</div>
 				{data.id === -1
 					? <></>
-					: <Input name="messageInput" placeholder='Enter your message...' className={styles['text-area']} onChange={onChange} onKeyDown={onSendMessage} value={value.messageInput}/>}
+					: <textarea
+						name='messageInput'
+						placeholder='Enter your message...'
+						className={styles['text-area']}
+						onChange={onChange}
+						onKeyDown={onSendMessage}
+						value={value.messageInput}/>}
+				{/*: <Input name="messageInput" placeholder='Enter your message...' className={styles['text-area']} onChange={onChange} onKeyDown={onSendMessage} value={value.messageInput}/>}*/}
 			</div>
 		</div>
 	);
