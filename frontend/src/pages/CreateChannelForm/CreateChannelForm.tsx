a0afcf9f783bccbc8003b7d2d2fbaa3e19b116c6
root <root@localhost.localdomain>
2023-10-09 09:24:19 -0400

    WIP

--- frontend/src/pages/CreateChannelForm/CreateChannelForm.tsx
+++ frontend/src/pages/CreateChannelForm/CreateChannelForm.tsx
@@ -17,12 +17,18 @@ export function CreateChannelFrom() {
 	const user = useSelector((s: RootState) => s.user);
 	const channelState = useSelector((s: RootState) => s.channel);
 	const [isProtected, setIsProtected] = useState<boolean>(false);
+	const [picture, setPicture] = useState<string>('/default_channel_public.png');
 	const onChange = (e: FormEvent<HTMLFormElement>) => { //FIXME!
 		console.log(e.currentTarget.type.value);
-		if (e.currentTarget.type.value === 'protected') {
-			setIsProtected(true);
-		} else {
+		if (e.currentTarget.type.value === 'public') {
 			setIsProtected(false);
+			setPicture('/default_channel_public.png')
+		} else if (e.currentTarget.type.value === 'protected') {
+			setIsProtected(true);
+			setPicture('/default_channel_protected.png')
+		} else if (e.currentTarget.type.value === 'private') {
+			setIsProtected(false);
+			setPicture('/default_channel_private.png')
 		}
 	};
 	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -41,8 +47,41 @@ export function CreateChannelFrom() {
 		//console.log(newChannel);
 	};
 
+	const updateAvatar = (e: FormEvent<HTMLInputElement>) => {
+		const target = e.target as HTMLInputElement;
+		const user_id = Number(getCookie('userId'))
+				if (user_id && target.files && target.files.length) {
+			const avatar = target.files[0];
+
+			const formData = new FormData();
+			formData.append('avatar', avatar, );
+			const file_name = dispatch(uploadAvatar(formData));
+						console.log(`Filename: ${file_name}`);
+			const old_filename = target.files[0].name;
+			const extension = old_filename.split('.').pop()
+			let update_user: UpdateUser = {
+				id: user.profile?.id,
+				username: user.profile?.username, //FIXME!!! username from the form
+				avatar: `http://${import.meta.env.VITE_BACK_HOST}:${import.meta.env.VITE_BACK_PORT}/user/avatars/` + user_id + '.png', //FIXME!!! avatar from the form
+				prefferedTableSide: user.profile?.prefferedTableSide,
+				pongColorScheme: user.profile?.pongColorScheme,
+			};
+			
+			dispatch(updateProfile(update_user));
+			window.location.reload(false);
+		}
+	}
+
+
 	return <>
 		<form className={styles['form']} onChange={onChange} onSubmit={onSubmit}>
+			<div className={styles['avatar_setting']}>
+				<img className={styles['avatar']} src={picture}/>
+				<div className={styles['middle_settings']}>
+					<input accept='image/png, image/jpeg, image/jpg' type="file" id='avatar_input' onChange={updateAvatar} hidden/>
+					<label htmlFor='avatar_input'><img src='/settings-fill.svg' alt='settings' className={styles['svg']}/></label>
+				</div>
+			</div>
 			<Headling>Create a new channel</Headling>
 			{channelState.error ? <div>Error!</div> : <></>}
 			<Input className={styles['input']} type='text' name='name' placeholder='Channel name' autoComplete='off'/>
