adbbbb4a0cf7d0059849adf586a49c2f0d5ed7fd
uru <muru@me.com>
2023-10-11 19:37:00 +0300

    ver run on local

--- backend/prisma/migrations/20230922091751_game_id_default_added/migration.sql
+++
@@ -1,7 +0,0 @@
--- DropIndex
-DROP INDEX "Game_id_key";
-
--- AlterTable
-CREATE SEQUENCE game_id_seq;
-ALTER TABLE "Game" ALTER COLUMN "id" SET DEFAULT nextval('game_id_seq');
-ALTER SEQUENCE game_id_seq OWNED BY "Game"."id";
