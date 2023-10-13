0c099c61bf964e57266ae730291c98e959c56162
uru <muru@me.com>
2023-10-13 19:26:51 +0300

    deploy done

--- frontend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /frontend
-npm install -g npm@10.0.0
-npm i
-npm run dev
--- pong/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /pong
-npm install -g npm@10.0.0
-npm install -g typescript
-npm i
-tsc
-npm start
